f39c75369de716b829deb589d39529e1c01ade1a
kweonminsung <kevin6528@scg.skku.ac.kr>
2022-07-21 05:51:44 +0900

    fix(devops) fix backurl

--- frontend/src/constants/endpoints.ts
+++ frontend/src/constants/endpoints.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL: string = 'http://enlistpedia.org/api';
+export const SERVER_URL: string = 'http://3.36.158.156:3001';
 // export const SERVER_URL: string = 'http://localhost:3001';
