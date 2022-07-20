624d233e78a0a28bc09dc90975bea6b55fafbde6
kweonminsung <kevin6528@scg.skku.ac.kr>
2022-07-21 05:46:54 +0900

    fix(chore) fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 pm2 delete all
 pm2 start "serve -s frontend/build -l 3000"
 cd backend
-pm2 start "uvicorn main:app --host 0.0.0.0 --port 3001"
+pm2 start "python3 -m uvicorn main:app --host 0.0.0.0 --port 3001"
 cd ..
